b1a5f0fbcd5bde426add30424c34a6cebeb5ffda
Nauval-Alghiffary <nauval.gif@gmail.com>
2021-03-03 13:29:43 +0700

    pertemuan 4

---
+++ resources/views/tryblade/layout.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>App Name - @yiels('title')</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        @section('sidebar')
+            This is the master sidebar
+        @show
+
+        <div class="container">
+            <h1>halo</h1>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/post', [PostController::Class, 'index']);
+Route::get('/', function () {
+    return view('tryblade.layout');
+});
